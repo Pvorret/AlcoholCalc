73675bfbad9bba3678b605b2dc47620de3ef66f3
Pvorret <phvorret@gmail.com>
2015-08-31 12:42:49 +0200

    Mere Done

---
+++ AlcoholCalculator/Login/Login.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    public class Login
+    {
+        private Person.Person P;
+        private string _userName;
+        private string _password;
+        public Dictionary<string, string> _users;
+
+        public Login()
+        {
+
+        }
+        public Login(Person.Person p)
+        {
+            _users = new Dictionary<string, string>();
+            this.P = p;
+        }
+
+        public string CreateUser(string userName, string password)
+        {
+            string response;
+            if (Regex.IsMatch(userName, @"^[a-zA-Z0-9]+$") && Regex.IsMatch(password, @"^[a-zA-Z0-9]+$"))
+            {
+                if(userName.Length >= 2 && password.Length >= 6 && password.Length <= 10)
+                {
+                    _users.Add(userName, password);
+                    response = "User created";
+                }
+                else
+                {
+                    response = "You need input a better UserName or Password";
+                } 
+            }
+            else
+            {
+                response = "You need input a better UserName or Password";
+            }
+            return response;
+
+        }
+
+        public bool LoginResponse(string userName, string password)//Find på et bedre navn
+        {
+            bool loginSuccess = false;
+
+            foreach (KeyValuePair<string, string> s in _users)
+            {
+                if (s.Key == userName && s.Value == password)
+                {
+                    loginSuccess = true;
+                }
+                else
+                {
+                    loginSuccess = false;
+                }
+            }
+
+            return loginSuccess;
+        }
+    }
+}
---
+++ AlcoholCalculator/LoginTest/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+
+namespace LoginTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Login.Login l = new Login.Login(new Person.Person("Phillip"));
+
+        [TestMethod]
+        public void U_Login_CreateNewUser_Name_Success()
+        {
+            bool failed = false;
+            
+            try
+            {
+                l.CreateUser("Pvorret", "Hej1");   
+            }
+            catch (Exception e)
+            {
+                Debug.Print(e.Message);
+                failed = true;
+            }
+            Assert.IsFalse(failed);
+        }
+        [TestMethod]
+        public void U_Login_CreateUser_Empty()
+        {
+            string response;
+            try
+            {
+                response = l.CreateUser("", "");   
+            }
+            catch (Exception e)
+            {
+                Debug.Print(e.Message);
+                response = "No";
+            }
+            Assert.AreEqual("You need input a better UserName or Password", response);
+
+        }
+        [TestMethod]
+        public void U_Login_LoginResponse_Empty()
+        {
+            bool failed = true;
+            try
+            {
+                failed = l.LoginResponse("", "");
+            }
+            catch (Exception e)
+            {
+                Debug.Print(e.Message);
+                failed = false;
+            }
+
+            Assert.IsFalse(failed);
+        }
+        [TestMethod]
+        public void U_Login_LoginRespons()
+        {
+            bool worked;
+
+            try
+            {
+                l._users.Add("Pvorret", "Hej1");
+                worked = l.LoginResponse("Pvorret", "Hej1");
+            }
+            catch (Exception e)
+            {
+                Debug.Print(e.Message);
+                worked = false;
+            }
+            Assert.IsTrue(worked);
+        }
+
+
+    }
+}
