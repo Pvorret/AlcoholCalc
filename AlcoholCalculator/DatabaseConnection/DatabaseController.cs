6e5ced0f3293ba19cbe9b34eb3470f51facb54a9
Sebastian F. Jørgensen <sebastianfalborg@gmail.com>
2015-08-31 13:45:46 +0200

    3 unit tests done

---
+++ AlcoholCalculator/DatabaseConnection/DatabaseController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DatabaseConnection
+{
+	public static class DatabaseController
+	{
+		static SqlConnection connection;
+
+		public static bool OpenConnection()
+		{
+			connection = 
+				new SqlConnection("server=ealdb1.eal.local;uid=ejl10_usr;" +
+            "pwd=Baz1nga10;database=EJL10_DB;");
+			try
+			{
+				connection.Open();
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+
+		}
+		public static void CloseConnection()
+		{
+			connection.Close();
+		}
+
+		public static bool DoUserExist(string user)
+		{
+			try
+			{
+				OpenConnection();
+
+				SqlCommand cmd = new SqlCommand("if exists(select id from " + 
+					"stpn_users where [USER] = @USER) select 1 else " +
+					"select 0", connection);
+				cmd.Parameters.Add("@USER", SqlDbType.NVarChar).Value = user;
+				cmd.ExecuteNonQuery();
+
+				SqlDataReader reader = cmd.ExecuteReader();
+
+				bool result = Convert.ToBoolean(reader.Read());
+
+				CloseConnection();
+
+				return result;
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+
+		public static bool CreateNewUser(string user)
+		{
+			return true;
+		}
+	}
+}
---
+++ AlcoholCalculator/DatabaseConnectionTests/DatabaseConnectionTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+using DatabaseConnection;
+using System.Data;
+
+namespace DatabaseConnectionTests
+{
+	[TestClass]
+	public class DatabaseConnectionTest
+	{
+		[TestMethod]
+		public void U_DatabaseConnection_Connection()
+		{
+			Assert.AreEqual(true, DatabaseController.OpenConnection());
+
+			DatabaseController.CloseConnection();
+		}
+
+		[TestMethod]
+		public void U_DatabaseConnection_DoUserExist_UserString()
+		{
+			SqlConnection connection =
+				new SqlConnection("server=ealdb1.eal.local; uid = ejl10_usr; " +
+						"pwd=Baz1nga10;database=EJL10_DB;");
+
+			connection.Open();
+
+			SqlCommand cmd = new SqlCommand("INSERT INTO stpn_users ([USER])" +
+				"values (@USER)", connection);
+			cmd.Parameters.Add("@USER", SqlDbType.NVarChar).Value = "test test";
+			cmd.ExecuteNonQuery();
+			connection.Close();
+
+			Assert.AreEqual(true, DatabaseController.DoUserExist("test test"));
+
+			Assert.AreNotEqual(false, DatabaseController.DoUserExist("test2 test2"));
+
+			connection.Open();
+
+			cmd = new SqlCommand("DELETE FROM stpn_users " +
+				"WHERE [USER] = 'test test'", connection);
+			cmd.ExecuteNonQuery();
+
+			connection.Close();
+    }
+
+    [TestMethod]
+		public void U_DatabaseConnection_CreateNewUser_UserString()
+		{
+			DatabaseController.CreateNewUser("test test");
+
+			SqlConnection connection =
+				new SqlConnection("server=ealdb1.eal.local; uid = ejl10_usr; " +
+						"pwd=Baz1nga10;database=EJL10_DB;");
+
+			connection.Open();
+
+			string result = 
+
+
+		}
+	}
+}
